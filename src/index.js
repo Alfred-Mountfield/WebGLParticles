fbbafbd39174e2442df3e842f2a7f39527fc2abd
Alfred <alfredmountfield@gmail.com>
2020-10-21 12:51:56 +0100

    Setup basic Babel, Webpack, Three project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import {BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer} from "three";
+
+const scene = new Scene()
+const camera = new PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const renderer = new WebGLRenderer()
+renderer.setSize( window.innerWidth, window.innerHeight )
+
+const gl = renderer.getContext()
+document.body.appendChild( renderer.domElement );
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+}
+animate();
